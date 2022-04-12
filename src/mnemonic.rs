bae596f178645baedd8af8af12add687726d6474
antoninhrlt <antoninhrlt1@gmail.com>
2022-04-12 23:27:30 +0200

    Update mnemonics

--- src/mnemonic.rs
+++ src/mnemonic.rs
@@ -4,6 +4,8 @@
 
 use std::string::ToString;
 
+use crate::label::Label;
+
 /// Also called as "instruction" (but keep as note that an instruction contains
 /// the mnemonic + operands, it's just a simplified way to say that and I never
 /// use this way to speak in this library), a mnemonic is designed to aid the 
@@ -13,14 +15,80 @@ use std::string::ToString;
 /// Complete list here : https://cs.brown.edu/courses/cs033/docs/guides/x64_cheatsheet.pdf
 #[derive(Debug)]
 pub enum Mnemonic {
+    Label(Label),
+
     Mov,
+    Push,
+    Pop,
+    Cwtl,
+    Cltq,
+    Cqto,
+    Inc,
+    Neg,
+    Not,
+    Leaq,
+    Add,
+    Sub,
+    Imul,
+    Xor,
+    Or,
+    And,
+    Sal,
+    Shl,
+    Sar,
+    Shr,
+    Imulq,
+    Mulq,
+    Idivq,
+    Divq,
+    Cmp,
+    Test,
+    Sete, Setz,
+    Setne, Setnz,
+    Sets, 
+    Setns,
+    Setg, Setnle,
+    Setge, Setnl,
+    Seta, Setnbe,
+    Setae, Setnb,
+    Setb, Setnae,
+    Setbe, Setna,
+    Jmp,
+    Je, Jz,
+    Jne, Jnz,
+    Js,
+    Jns,
+    Jg, Jnle,
+    Jge, Jnl,
+    Jl, Jnge,
+    Jle, Jng,
+    Ja, Jnbe,
+    Jae, Jnb,
+    Jb, Jnae,
+    Jbe, Jna,
+    Cmove, Cmovz,
+    Cmovne, Cmovnz,
+    Cmovs,
+    Cmovns,
+    Cmovg, Cmovnle,
+    Cmovge, Cmovnl,
+    Cmovl, Cmovnge,
+    Cmovle, Cmovng,
+    Cmova, Cmovnbe,
+    Cmovae, Cmovnb,
+    Cmovb, Cmovnae,
+    Cmovbe, Cmovna,
+    Call,
+    Leave,
+    Ret
 }
 
+/// Convert the enum object identifier to a string as lowercase
 impl ToString for Mnemonic {
     fn to_string(&self) -> String {
-        match *self {
-            Mnemonic::Mov => "mov",
+        match self {
+            Mnemonic::Label(label) => label.to_string(),
+            _ => format!("{:?}", self).to_lowercase()
         }
-        .to_string()
     }
 }
